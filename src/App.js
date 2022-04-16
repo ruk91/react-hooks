76524c2855e3f9c629ea861fb97a6b6f73d16d0c
sajithkasn@gmail.com <you@example.com>
2022-04-16 14:11:36 +0530

    🎉 First commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React, { useState } from 'react';
+import './App.css';
+
+function App() {
+
+  const [ count, setCount] = useState(0)
+  return (
+    <div className="App">
+      <p>You clicked {count} times!!!</p>
+      <button onClick={() => setCount(count+1)}>Click here!</button>
+    </div>
+  );
+}
+
+export default App;
